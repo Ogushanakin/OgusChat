904a3fcfaa1ed1755576d5c707d0d63161cb1659
Ogushanakin <ogzhnakn96@gmail.com>
2022-07-03 12:42:04 +0300

    first upload

---
+++ OgusChat/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OgusChat
+//
+//  Created by AKIN on 2.07.2022.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
