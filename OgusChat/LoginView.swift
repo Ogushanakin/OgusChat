71e45e4fb56baa4f301c6e64c8b94c4616aa7140
Ogushanakin <ogzhnakn96@gmail.com>
2022-07-27 20:17:32 +0300

    Change framework to SwiftUI and add Login Page

---
+++ OgusChat/LoginView.swift
@@ -0,0 +1,81 @@
+//  ContentView.swift
+//  OgusChat
+//
+//  Created by AKIN on 27.07.2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker Here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    if !isLoginMode{
+                        Button {
+                            
+                        } label: {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 180, height: 180)
+                        }
+                    }
+                    
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(Color.white)
+                    
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }.background(Color.blue)
+                    }
+                }
+                .padding()
+                
+            }
+            .background(Color(.init(white: 0, alpha: 0.05)) .ignoresSafeArea())
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+        }
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            print("Should log into Firebase with existing credentials")
+        } else {
+            print("Register a new account inside of Firebase Auth and then store image in Storage somehow...")
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ OgusChat/OgusChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  OgusChatApp.swift
+//  OgusChat
+//
+//  Created by AKIN on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct OgusChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
